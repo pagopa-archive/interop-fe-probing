8a0c8e609e4d1da2896f4f92651d60415864af25
epetkova2 <epetkova2@dxc.com>
2023-05-26 17:01:49 +0300

    fix test

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: {},
-  },
   resolve: {
     alias: {
       './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
     },
   },
   test: {
