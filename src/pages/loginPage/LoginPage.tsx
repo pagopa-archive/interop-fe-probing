918f6fdee98f1b2ccc77ea3b0952ebb0331d102b
epetkova2 <epetkova2@dxc.com>
2023-06-06 16:23:05 +0300

    pr changes

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -1,12 +1,10 @@
 import { LoginForm } from '../../components/forms/loginForm/LoginForm'
 import { Typography, Link, Stack } from '@mui/material'
 import { useTranslation } from 'react-i18next'
-import { useNavigate } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
 
 const LoginPage = () => {
   const { t } = useTranslation(['loginPage'])
-
-  const navigate = useNavigate()
 
   return (
     <Stack spacing={4} alignItems="center" component="main" my={9}>
@@ -16,7 +14,7 @@ const LoginPage = () => {
       <LoginForm />
       <Typography color="text.secondary">
         {t('forgotPassword1', { ns: 'loginPage' })}
-        <Link onClick={() => navigate('/recupero-password')} underline="none" component="button">
+        <Link underline="none" component={RouterLink} to="/recupero-password">
           {t('forgotPassword2', { ns: 'loginPage' })}
         </Link>
         {t('forgotPassword3', { ns: 'loginPage' })}
