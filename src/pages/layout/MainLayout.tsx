4b78384bdf7341903f335ee3bf38443a757bcc29
epetkova2 <epetkova2@dxc.com>
2023-06-22 16:00:26 +0300

    refresh token fix

--- src/pages/layout/MainLayout.tsx
+++ src/pages/layout/MainLayout.tsx
@@ -1,9 +1,8 @@
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import { logout, refreshToken } from '../../authetication/auth'
-import { useNavigate } from 'react-router-dom'
 
 /**
  * Main layout of the application with header and footer
@@ -13,27 +12,32 @@ const MainLayout = () => {
   const navigate = useNavigate()
 
   useEffect(() => {
-    //Refresh the access and id token every 59 minutes
-    const idTokenInterval = setInterval(async () => {
-      await refreshToken()
-    }, 3540000)
+    if (sessionStorage.getItem('token')) {
+      //Refresh the access and id token every 59 minutes
+      const idTokenInterval = setInterval(() => {
+        console.log('idTokenInterval')
+        refreshToken()
+      }, 3540000)
 
-    //Log the user out once the refresh token has expired (30 days)
-    const refreshTokenInterval = setInterval(async () => {
-      await logout()
-        .then(() => {
-          navigate('/login')
-        })
-        .catch((error: any) => {
-          throw error
-        })
-    }, 2592000000)
-
-    return () => {
-      clearInterval(idTokenInterval)
-      clearInterval(refreshTokenInterval)
+      //Log the user out once the refresh token has expired (30 days)
+      const refreshTokenInterval = setInterval(() => {
+        if (sessionStorage.getItem('token')) {
+          logout()
+            .then(() => {
+              navigate('/login')
+            })
+            .catch((error: any) => {
+              throw error
+            })
+        }
+      }, 2147483647) // around 24.8 days which is the max delay
+      // }, 2 592 000 000) //30 days - too large delay
+      return () => {
+        clearInterval(idTokenInterval)
+        clearInterval(refreshTokenInterval)
+      }
     }
-  }, [])
+  }, [sessionStorage.getItem('token')])
 
   return (
     <>
